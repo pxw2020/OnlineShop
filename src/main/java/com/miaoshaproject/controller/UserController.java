c0ab08bd1fb02388706ea6befce99280e999970e
xuwen Pang <419759309@qq.com>
2021-01-21 00:25:21 +0800

    opt验证码

--- src/main/java/com/miaoshaproject/controller/UserController.java
+++ src/main/java/com/miaoshaproject/controller/UserController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 /**
  * @author xuwen
@@ -29,6 +30,30 @@ public class UserController extends BaseController {
 
     @Autowired
     private UserServiceImpl userService;
+
+    @Autowired
+    private HttpServletRequest httpServletRequest;
+
+    //用户获取otp短信接口
+    @RequestMapping("/getotp")
+    public CommonReturnType getOtp(@RequestParam(name="telephone")String telephone) {
+        //按照一定规则生成otp验证码
+        Random random = new Random();
+        int randomInt = random.nextInt(99999);
+        randomInt+=10000;
+        String otpCode = String.valueOf(randomInt);
+
+        //TODO 将验证码和对应用户手机号关联(redis存储-适合分布式）
+        //暂时使用httpSession绑定opt和手机号
+        httpServletRequest.getSession().setAttribute(telephone,otpCode);
+
+        //TODO 通过短信渠道发送otp验证码（省略）
+
+        System.out.println(""+telephone+"  otp短信验证码："+otpCode);
+        return null;
+    }
+
+
 
     @RequestMapping("/get")
     public CommonReturnType getUser(@RequestParam(name="id")Integer id) throws BusinessException {
